10a9f00b145ddec93d1ce69dde8c793cee3466f0
hoanguyen <httt.dev@gmail.com>
2020-10-23 08:23:33 +0700

    Connect mongodb use mongoose lib

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { Route } from './core/interfaces';
+import mongoose from 'mongoose';
+
 class App{
     public app : express.Application;
     public port : string|number;
@@ -8,6 +10,7 @@ class App{
         this.app = express();
         this.port = process.env.PORT || 5000;
         this.initializeRoutes(routes);
+        this.connectToDatabase();
     }
     private initializeRoutes(routes : Route[]){
         routes.forEach((route) => {
@@ -19,6 +22,22 @@ class App{
         this.app.listen(this.port , ()=>{
             console.log(`Server is listening on port ${this.port}`); 
         })
+    }
+
+    private async connectToDatabase(){
+        try{
+            const connectionString  = "mongodb+srv://social:Abc12345@master.dlu4d.mongodb.net/social?retryWrites=true&w=majority";
+            await mongoose.connect(connectionString, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true
+              });
+              console.log("Connect DB successful")
+        }
+        catch(error){
+            console.log("Connect DB error")
+        }
     }
 }
 
